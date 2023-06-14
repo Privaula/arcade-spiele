87baf1c7006cfbf74994e54371a1a3fc53260621
Julian <43094366+Privaula@users.noreply.github.com>
2023-06-14 15:47:34 +0200

    Update zahlenguesser.java

--- src/zahlenguesser.java
+++ src/zahlenguesser.java
@@ -9,6 +9,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 
 
@@ -23,12 +24,12 @@ public class zahlenguesser extends JFrame {
 	public static void lul() {
 		// TODO Auto-generated method stub
 		
-		int random = (int)(Math.random() * 10);
+		int random = (int)(Math.random() * 100);
 		System.out.println(random);
 		
 		
 		JFrame jf = new JFrame();
-		System.out.println("siuu");
+		System.out.println("siuus");
 		jf.setTitle("Tolle GUI");
 		jf.setSize(1280, 720);
 		jf.setLocationRelativeTo(null);
@@ -41,18 +42,37 @@ public class zahlenguesser extends JFrame {
 		myLabel.setSize(1280, 720);
 		
 		//random number anzeigen
-		JLabel randomNumber = new JLabel("random" + random);
+		JLabel randomNumber = new JLabel("random " + random);
 		randomNumber.setSize(500, 70);
 		randomNumber.setLocation(50, 50);
 		Font schrift = new Font ("Serif", Font.BOLD, 80);
 		randomNumber.setFont(schrift);
-		
+
+		//Frage
+		String guess = JOptionPane.showInputDialog("Bitte wählen sie eine Zahl von 1 bis 100 aus.");
+		System.out.println(guess);
+		int newGuess = Integer.parseInt(guess);
 		jf.add(randomNumber);
 		jf.add(myLabel);
-		
-		
-		
+
+		//Überprüfung
+		if (random == newGuess) {
+			System.out.println("richtig");
+			
+
+		}else if(newGuess < random){
+			System.out.println("Die Zahl ist größer.");
+			guess = JOptionPane.showInputDialog("Bitte wählen sie eine Zahl von 1 bis 100 aus.");
+		}else if(newGuess > random){
+			System.out.println("Die Zahl ist kleiner.");
+			guess = JOptionPane.showInputDialog("Bitte wählen sie eine Zahl von 1 bis 100 aus.");
+		}
+	
 	}
+		
+		
+		
+	
 	
 
 }
