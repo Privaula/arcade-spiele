66cd12b4dce00d815c1fa2e61a35e443ec910514
Julian <43094366+Privaula@users.noreply.github.com>
2023-06-16 23:54:53 +0200

    Update memory.java  fixing ende button problem

--- src/memory.java
+++ src/memory.java
@@ -1,6 +1,18 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class memory {
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class memory extends JFrame {
 	
 	public static void Main(String[] args) {
 		
@@ -9,12 +21,31 @@ public class memory {
 		public static void memory() {
 			
 			JFrame jf = new JFrame();
-			jf.setTitle("Tolle GUI");
+			jf.setTitle("Arcade Spiele - Memory");
 			jf.setSize(1280, 720);
 			jf.setLocationRelativeTo(null);
 			jf.setVisible(true);
 			System.out.println("ja von memory");
 		
-		
+			
+			
+			JButton endeButton = new JButton("Ende");
+			endeButton.setBounds(30, 40, 70, 50);
+			endeButton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.exit(0);	
+				}
+			});
+			
+			
+			
+			GroupLayout gl = new GroupLayout(jf);
+			jf.setLayout(null);
+			
+			
+			//add to jf
+			jf.add(endeButton);
 		}
 }
