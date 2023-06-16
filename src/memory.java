2c0ca59bc24b0594df5171818cef9fa41debf46d
Julian <43094366+Privaula@users.noreply.github.com>
2023-06-16 17:08:08 +0200

    memory
    für aman und jonas

---
+++ src/memory.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class memory {
+	
+	public static void Main(String[] args) {
+		
+	}
+		
+		public static void memory() {
+			
+			JFrame jf = new JFrame();
+			jf.setTitle("Tolle GUI");
+			jf.setSize(1280, 720);
+			jf.setLocationRelativeTo(null);
+			jf.setVisible(true);
+			System.out.println("ja von memory");
+		
+		
+		}
+}
