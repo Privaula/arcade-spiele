7f9e9ed8d71a0562d3107d7713564695fe941299
Julian <43094366+Privaula@users.noreply.github.com>
2023-06-25 21:03:41 +0200

    Update rps.java playable now

--- src/rps.java
+++ src/rps.java
@@ -2,7 +2,9 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
+import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,6 +17,8 @@ public class rps {
 	static int buttonHöhe = Main.buttonHöhe;
 	static String eingabe;
 	static String randomString;
+	static String randomStringName;
+	static String computerMessage;
 	
 	public static void Main(String[]args) {
 		
@@ -24,12 +28,10 @@ public class rps {
 		
 		
 		int random = (int)(Math.random() * 3 +1);
-		System.out.println(random);
-		
 		
 		//JFrame erstellen
 		JFrame jf = new JFrame();
-		jf.setTitle("Arcade Spiele - Memory");
+		jf.setTitle("Arcade Spiele - Schere Stein Papier");
 		jf.setSize(1280, 720);
 		jf.setLocationRelativeTo(null);
 		jf.setVisible(true);
@@ -53,6 +55,18 @@ public class rps {
 					}
 		});
 		
+		//nochmal Button
+		JButton nochmalButton = new JButton("Nochmal spielen!");
+		nochmalButton.setBounds(170, 480, 140, buttonHöhe);
+		nochmalButton.setVisible(false);
+		nochmalButton.addActionListener(new ActionListener() {
+					
+					@Override
+			public void actionPerformed(ActionEvent e) {
+				jf.setVisible(false);
+				rps.rps();
+					}
+		});
 
 		
 		//Text Anzeige
@@ -67,8 +81,20 @@ public class rps {
 		rpsLabel.setFont(schrift);
 		
 		
+		//PC Button
+		//"Der Computer hat "+ randomStringName + " gewählt." + computerMessage
+		JButton computerButton = new JButton("Wähle eine der drei Optionen aus.");
+		computerButton.setBounds(360, 420, 400, 200);
+		computerButton.addActionListener(new ActionListener() {
+							@Override
+				public void actionPerformed(ActionEvent e) {
+							
+						}
+		});
+		
+		
 		//Rock Button
-		JButton rockButton = new JButton("Rock");
+		JButton rockButton = new JButton("Stein");
 		rockButton.setBounds(200, 170, 200, 200);
 		rockButton.addActionListener(new ActionListener() {
 			@Override
@@ -76,53 +102,64 @@ public class rps {
 			public void actionPerformed(ActionEvent e) {
 				//System.out.println("Stein");
 				rps.eingabe = "r";
-				System.out.println(eingabe);
 				proof();
+				computerButton.setText("Der Computer hat "+ randomStringName + " gewählt." + " Sie haben "+ computerMessage + ".");
+				nochmalButton.setVisible(true);
 			}
 		});
 		
 	
 		
 		//Paper Button
-		JButton paperButton = new JButton("Paper");
+		JButton paperButton = new JButton("Papier");
 		paperButton.setBounds(460, 170, 200, 200);
 		paperButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//System.out.println("Papier");
 				rps.eingabe = "p";
-				System.out.println(eingabe);
 				proof();
+				computerButton.setText("Der Computer hat "+ randomStringName + " gewählt." + " Sie haben "+ computerMessage + ".");
+				nochmalButton.setVisible(true);
 			}
 		});
 		
 		// Scissors Button
-		JButton scissorsButton = new JButton("Scissors");
+		JButton scissorsButton = new JButton("Schere");
 		scissorsButton.setBounds(720, 170, 200, 200);
 		scissorsButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//System.out.println("Schere");
 				rps.eingabe = "s";
-				System.out.println(eingabe);
 				proof();
+				computerButton.setText("Der Computer hat "+ randomStringName + " gewählt." + " Sie haben "+ computerMessage + ".");
+				nochmalButton.setVisible(true);
 			}
 		});
 		
+		
+				
+		
+		
+		
 		switch(random) {
 		case 1:
-			System.out.println("Stein");
+			System.out.println("PC hat Stein");
 			randomString = "r";
+			randomStringName = "Stein";
 			break;
 		
 		case 2:
-			System.out.println("Papier");
+			System.out.println("PC hat Papier");
 			randomString = "p";
+			randomStringName = "Papier";
 			break;
 		
 		case 3:
-			System.out.println("Schere");
+			System.out.println("PC hat Schere");
 			randomString = "s";
+			randomStringName = "Schere";
 			break;
 		}
 		System.out.println();
@@ -130,12 +167,9 @@ public class rps {
 		
 		
 		
-		
-		
-		
-		
-		
 		//add to jf
+		jf.add(nochmalButton);
+		jf.add(computerButton);
 		jf.add(zurückButton);
 		jf.add(scissorsButton);
 		jf.add(paperButton);
@@ -144,17 +178,48 @@ public class rps {
 		jf.add(myLabel);
 	
 		
+		
 	}
 	static void proof() {
-		//Ergebnisüberprüfung
+		//Ergebnisüberprüfung Rock
 		if(eingabe == "r" && randomString == "r") {
-			System.out.println("Unentschieden");
+			System.out.println("unentschieden");
+			rps.computerMessage = "unentschieden";
+			
 		}
 		if(eingabe == "r" && randomString == "p") {
-			System.out.println("Verloren");
+			System.out.println("verloren");
+			rps.computerMessage = "verloren";
 		}
 		if(eingabe == "r" && randomString == "s") {
-			System.out.println("Gewonnen");
+			System.out.println("gewonnen");
+			rps.computerMessage = "gewonnen";
+		}
+		//Ergebnisüberprüfung Paper
+		if(eingabe == "p" && randomString == "r") {
+			System.out.println("gewonnen");
+			rps.computerMessage = "gewonnen";
+		}
+		if(eingabe == "p" && randomString == "p") {
+			System.out.println("unentschieden");
+			rps.computerMessage = "unentschieden";
+		}
+		if(eingabe == "p" && randomString == "s") {
+			System.out.println("verloren");
+			rps.computerMessage = "verloren";
+		}
+		//Ergebnisüberprüfung Scissors
+		if(eingabe == "s" && randomString == "r") {
+			System.out.println("verloren");
+			rps.computerMessage = "verloren";
+		}
+		if(eingabe == "s" && randomString == "p") {
+			System.out.println("gewonnen");
+			rps.computerMessage = "gewonnen";
+		}
+		if(eingabe == "s" && randomString == "s") {
+			System.out.println("unentschieden");
+			rps.computerMessage = "unentschieden";
 		}
 	}
 	
