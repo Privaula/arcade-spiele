5bc4a4407fb3d12634219f2d2f0d9a36d71c36c4
VitaVate187 <134417341+VitaVate187@users.noreply.github.com>
2023-06-30 17:35:23 +0200

    Update timeThread.java
    added date

--- src/timeThread.java
+++ src/timeThread.java
@@ -20,41 +20,6 @@ public class timeThread extends Thread{
 	
 	public void run()
 	{
-//		System.out.println("customThread gestarted");
-//		
-//		do 
-//		{
-//			boolean b = false;
-//			do
-//			{		
-//				try {
-//					Thread.sleep(500);
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}		
-			/*if(date2.format(new Date()).toString() != date1.format(new Date()).toString())
-			{
-				System.out.println(date1.format(new Date()));
-				System.out.println(date2.format(new Date()));
-				date1 = new SimpleDateFormat("HH:mm");
-				//System.out.println(date1.format(new Date()));
-				//main.
-			}	
-//			*/			
-//			}while(b = false);
-//			
-//			
-//			
-//			
-//		}
-//		while(Main.timerRunning);
-		
-		String a = "h" + "allo";
-		String b = "hallo";
-		
-		a.equals(b);
-		
 		boolean running = true;
 		
 		while(running)
@@ -81,6 +46,7 @@ public class timeThread extends Thread{
 			public void run() {
 				
 				_mainInstanz.updateTimeLabel();
+				_mainInstanz.updateDateLabel();
 				
 			}
 		});
