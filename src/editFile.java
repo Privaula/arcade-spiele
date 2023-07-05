f3c159b0a6dea4af65a29a1741ac45ade13b48aa
VitaVate187 <134417341+VitaVate187@users.noreply.github.com>
2023-07-05 11:07:44 +0200

    Add files via upload

---
+++ src/editFile.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
+
+
+//public class editFile {
+//	public void createFile()
+//	{
+//		try {
+//			
+//		Class<? extends editFile> directory = this.getClass();
+//		System.out.println(directory);
+//		//var playtimeFile = new File("playtime_arcadeSpiele");
+//		if(playtimeFile.createNewFile())
+//		{
+//			
+//			System.out.println("file created");
+//		}
+//		}
+//		catch(IOException e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+//}
