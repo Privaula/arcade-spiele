9476d5eda5dbdf07224ccde968343a789ddc772a
VitaVate187 <134417341+VitaVate187@users.noreply.github.com>
2023-07-06 20:38:44 +0200

    Update editFile.java

--- src/editFile.java
+++ src/editFile.java
@@ -1,25 +1,80 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 
-//public class editFile {
-//	public void createFile()
-//	{
-//		try {
-//			
-//		Class<? extends editFile> directory = this.getClass();
-//		System.out.println(directory);
-//		//var playtimeFile = new File("playtime_arcadeSpiele");
-//		if(playtimeFile.createNewFile())
-//		{
-//			
-//			System.out.println("file created");
-//		}
-//		}
-//		catch(IOException e)
-//		{
-//			e.printStackTrace();
-//		}
-//	}
-//}
+public class editFile {
+	static int sff;
+	static int mff;
+	static int hff;
+	public static void createFile()
+	{
+		
+		String str = System.getenv("APPDATA");
+		File gamedata = new File(str + "/arcade-spiele/gamedata");
+		System.out.println(str);
+		if(!gamedata.exists())
+		{
+			gamedata.mkdirs();
+			File playTime = new File(str + "/arcade-spiele/gamedata/playtime.txt");
+			try {
+				playTime.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("fortnite2");
+		}
+	}
+	public static void editFile(int s, int m, int h)
+	{
+		String str = System.getenv("APPDATA");
+		FileWriter myWriter;
+		try {
+			myWriter = new FileWriter(str + "/arcade-spiele/gamedata/playtime.txt");
+			myWriter.write(s + "\n" + m + "\n" + h);
+			
+			
+//			System.out.println("File: " + str + "/arcade-spiele/gamedata/playtime.txt");
+//			System.out.println(myWriter);
+//			myWriter.write("Files in Java might be tricky, but it is fun enough!4");
+		    myWriter.close();
+		} catch (IOException e) {
+			System.out.println("An Error is occurred while trying to edit the file");
+		}
+	}
+	public static void readFile()
+	{
+		String str = System.getenv("APPDATA");
+		File gamedata = new File(str + "/arcade-spiele/gamedata/playtime.txt");
+		//System.out.println(str);
+		try {
+			Scanner scanner = new Scanner(gamedata);
+			String string = "42";
+			int inte = Integer.parseInt(string);
+		
+			String sStr = scanner.nextLine();
+			
+			Integer inte2 = Integer.parseInt(str);
+//			System.out.println(inte2);
+			
+//			sff = Integer.parseInt(sStr);
+//			String mStr = scanner.nextLine();
+//			mff = Integer.parseInt(mStr);
+//			String hStr = scanner.nextLine();
+//			hff = Integer.parseInt(hStr);
+//			System.out.println("werte gespeichert");
+//			System.out.println(sStr);
+//			System.out.println(mStr);
+//			System.out.println(hStr);
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("konnte nicht ausgelesen werden");
+		}
+	}
+	
+}
+
